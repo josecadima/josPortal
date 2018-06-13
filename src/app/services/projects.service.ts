49b8ed9a0ee0de2a529ab0c1a5e3b16e31a068b3
Cadima <jose.cadima@affiliate.oliverwyman.com>
2018-06-13 19:44:48 -0400

    Implement Multi configuration environments

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,5 +1,6 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core'
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ProjectsService {
@@ -10,14 +11,14 @@ export class ProjectsService {
     constructor (private http: Http) { }
 
     getProjects() {
-        this.http.get('http://localhost:3000/projects').subscribe(res => {this.projects = res.json();});
+        this.http.get(environment.josPortalApiUrl + 'projects').subscribe(res => {this.projects = res.json();});
     }
 
     getFunProjects() {
-        this.http.get('http://localhost:3000/funprojects').subscribe(res => {this.funProjects = res.json();});
+        this.http.get(environment.josPortalApiUrl + 'funprojects').subscribe(res => {this.funProjects = res.json();});
     }
 
     getProfessionalProjects() {
-        this.http.get('http://localhost:3000/professionalprojects').subscribe(res => {this.professionalProjects = res.json();});
+        this.http.get(environment.josPortalApiUrl + 'professionalprojects').subscribe(res => {this.professionalProjects = res.json();});
     }
 }
